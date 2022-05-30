4654e108c76105545703088f52dced1cad2fdcff
KevenyLima <kevenylima12345@hotmail.com>
2022-05-30 12:10:38 -0300

    complete challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,68 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import '../styles/sideBar.scss';
+import { GenreResponseProps } from "../interfaces/IGenreResponseProps";
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+
+interface sideBarProps {
+  setSelectedGenre:React.Dispatch<React.SetStateAction<GenreResponseProps>>
+  setMovies:React.Dispatch<React.SetStateAction<MovieProps[]>>
+}
+export default function SideBar({setSelectedGenre,setMovies}:sideBarProps) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
+  }, [selectedGenreId]);
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
